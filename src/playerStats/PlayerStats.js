e4a491df13770ed12849fcab85b19d50a69532dc
Cloud-Monkey <ironmonkeydaveb@yahoo.co.uk>
2024-07-02 11:54:13 +0100

    style: changed all nav bar icons to new style

--- src/playerStats/PlayerStats.js
+++ src/playerStats/PlayerStats.js
@@ -4,12 +4,12 @@ import "../styles/playerStats.css";
 const PlayerStats = () => {
   return (
     <div id="playerStats">
-      <h1>David Brown lvl 18</h1>
+      <h1>David Brown</h1>
       <div id="playerStats__lines">
         <span id="playerStats__lines__thick"></span>
         <span id="playerStats__lines__thin"></span>
       </div>
-      <h2>Junior Frontend Dev</h2>
+      <h2>Junior Fullstack Dev</h2>
     </div>
   );
 };
